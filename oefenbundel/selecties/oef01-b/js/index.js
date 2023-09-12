3d6786c936593a546a1d9d0e16c3b5daeccfb6ac
Neverlanders <vanhoutte.tom@gmail.com>
2023-09-12 16:06:49 +0200

    oefening 01b met html afgewerkt

---
+++ oefenbundel/selecties/oef01-b/js/index.js
@@ -0,0 +1,14 @@
+// Vraag de gebruiker om hun leeftijd
+let leeftijd = prompt("Voer uw leeftijd in:");
+
+// Controleer of de ingevoerde leeftijd minimaal 18 is
+if (leeftijd >= 18) {
+    // Toon bericht als de gebruiker oud genoeg is
+
+    console.log("Je bent " + leeftijd + " jaar oud. Je mag deelnemen aan de spelen van de nationale loterij.");
+} else {
+    // Toon bericht als de gebruiker niet oud genoeg is
+    console.log("Om deel te nemen aan de spelen van de nationale loterij moet je minimum 18 jaar oud zijn.");
+
+
+}
--- oefenbundel/selecties/oef01/js/index.js
+++ oefenbundel/selecties/oef01/js/index.js
@@ -1,14 +1,20 @@
+
+document.getElementById("controleerLeeftijd").onclick = function(){
 // Vraag de gebruiker om hun leeftijd
-let leeftijd = prompt("Voer uw leeftijd in:");
+    let leeftijd = document.getElementById("mijnLeeftijd").value;
 
 // Controleer of de ingevoerde leeftijd minimaal 18 is
-if (leeftijd >= 18) {
-    // Toon bericht als de gebruiker oud genoeg is
+    if (leeftijd >= 18) {
+        // Toon bericht als de gebruiker oud genoeg is
 
-    console.log("Je bent " + leeftijd + " jaar oud. Je mag deelnemen aan de spelen van de nationale loterij.");
-} else {
-    // Toon bericht als de gebruiker niet oud genoeg is
-    console.log("Om deel te nemen aan de spelen van de nationale loterij moet je minimum 18 jaar oud zijn.");
+        console.log("Je bent " + leeftijd + " jaar oud. Je mag deelnemen aan de spelen van de nationale loterij.");
+    } else {
+        // Toon bericht als de gebruiker niet oud genoeg is
+        console.log("Om deel te nemen aan de spelen van de nationale loterij moet je minimum 18 jaar oud zijn.");
 
 
+    }
 }
+
+
+
